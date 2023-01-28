02e1d6d2768efb808bf5c72c98a3031c315956b2
Youssef ElAsfoury <youssefelasfoury@gmail.com>
2023-01-28 17:55:01 +0200

    :sparkles: Init the app

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-full flex flex-col"></div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        primary: '#1D9DB1',
+      },
+    },
+  },
+}
